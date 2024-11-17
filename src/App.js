c28a86b1af1dc87d3a81d51ae18f5e01db9d3312
martyshe <sapognikmisha@gmail.com>
2024-11-17 09:55:06 +0100

    work18_11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import toDoList from './components/toDoList/ToDoList';
+import styles from './components/toDoList/ToDoList.module.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   toDoList.map( el => {
+    return(
+      <div className={styles.container}>
+        <div key={el.name} className={styles.cards}>
+        <h2>{el.name}</h2>
+        <p className={styles.deadline}>{el.deadline}</p>
+        <p>{el.describe}</p>
+      </div>
+      </div>
+      
+    )
+   })
   );
 }
 
---
+++ src/components/toDoList/ToDoList.jsx
@@ -0,0 +1,24 @@
+const toDoList = [
+    {
+        name: 'English',
+        deadline: 'yesterday',
+        describe: 'Programing without knowing English in Europe is practicarly impossible'
+    },
+    {
+        name: 'React',
+        deadline: '01.03.2025',
+        describe: 'Programing without React is practicarly impossible'
+    },
+    {
+        name: 'SQL',
+        deadline: '01.03.2025',
+        describe: 'Programing without knowing SQL in Europe is practicarly impossible'
+    },
+    {
+        name: 'Algorythms',
+        deadline: '01.03.2025',
+        describe: 'Programing without knowing Algorythms is practicarly impossible'
+    },
+];
+
+export default toDoList;
